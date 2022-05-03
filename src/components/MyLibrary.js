14d2c28f6708f87a11b7453cbdac072f070ea536
Alvin Chou <alvinchou516@hotmail.com>
2022-05-03 16:14:45 -0400

    docs: :memo: figured out a way to sort db list in to 3 categories
    need to come back to refactor

--- src/components/MyLibrary.js
+++ src/components/MyLibrary.js
@@ -6,64 +6,118 @@ import firebase from "../firebase";
 import Header from "./Header";
 
 export default function MyLibrary() {
-    const [myBooks, setMyBooks] = useState([]);
+    const [myBooks, setMyBooks] = useState({});
 
     useEffect(() => {
         const database = getDatabase(firebase);
         const dbRef = ref(database);
         onValue(dbRef, (res) => {
-            //create a new array, push items to it, set state to be new array
-            const newState = [];
+            //create a new array to sort the books, push items to it
             const toReadList = [];
+            const favList = [];
+            const haveReadList = [];
             const data = res.val();
 
-            console.log(data);
-            // if (data?.toRead) {
-                // }
-                
-                
-                for (let key in data) {
-                    console.log(data[key]);
-                    if (data[key].toRead) {
-                        console.log('toRead');
-                        toReadList.push(data[key].toRead)
+            // I think I can use .filter() here
+            for (let key in data) {
+                if (data[key].toRead) {
+                    console.log('toRead');
+                    toReadList.push(data[key].toRead)
+                } else if (data[key].fav) {
+                    favList.push(data[key].fav)
+                } else if (data[key].haveRead) {
+                    haveReadList.push(data[key].haveRead)
                 }
-                newState.push( data[key] );
             }
-            console.log(toReadList);
-            setMyBooks(newState)
+
+            // create a new object to hold the 3 arrays
+            const newState = {
+                toRead: toReadList,
+                fav: favList,
+                haveRead: haveReadList
+            }
+
+            setMyBooks(newState);
         })
     }, [])
 
-    // const toReadList = myBooks.filter((category) => category === 'toRead')
-    // console.log(toReadList);
-
-    // console.log(myBooks);
     return (
         <div className="myLibrary">
             <div className="wrapper">
                 <Header />
                 <h2>My Library</h2>
-                <ul className="toRead">
-                    {myBooks.map((book)=>{
-                        return (
-                            // <li key={book.id} className="bookCard">
-                            //     <div className="imgContainer">
-                            //         <img src={book.image} alt={`Book cover of ${book.title}`} />
-                            //     </div>
-                            //     <p>{book.title}</p>
-                            //     <p>{book.authors.map((author, index) => {
-                            //         return(
-                            //             index === 0
-                            //             ? author
-                            //             : `, ${author}`
-                            //         )
-                            //     })}</p>
-                            // </li>
-                            <li>Hi</li>
-                        )
-                    })}
+                <ul>
+                    <p>to Read</p>
+                    {myBooks.toRead
+                        ? myBooks.toRead.map((book) => {
+                            return (
+                                <li key={book.id} className="bookCard">
+                                    <div className="imgContainer">
+                                        <img src={book.image} alt={`Book cover of ${book.title}`} />
+                                    </div>
+                                    <p>{book.title}</p>
+                                    <p>{book.authors.map((author, index) => {
+                                        return(
+                                            index === 0
+                                            ? author
+                                            : `, ${author}`
+                                        )
+                                    })}</p>
+                                </li>
+                            )
+                        })
+                        : null
+                    }
                 </ul>
+
+                <ul>
+                    <p>fav</p>
+                    {myBooks.fav
+                        ? myBooks.fav.map((book) => {
+                            return (
+                                <li key={book.id} className="bookCard">
+                                    <div className="imgContainer">
+                                        <img src={book.image} alt={`Book cover of ${book.title}`} />
+                                    </div>
+                                    <p>{book.title}</p>
+                                    <p>{book.authors.map((author, index) => {
+                                        return(
+                                            index === 0
+                                            ? author
+                                            : `, ${author}`
+                                        )
+                                    })}</p>
+                                </li>
+                            )
+                        })
+                        : null
+                    }
+                </ul>
+
+                <ul>
+                    <p>have read</p>
+                    {myBooks.haveRead
+                        ? myBooks.haveRead.map((book) => {
+                            return (
+                                <li key={book.id} className="bookCard">
+                                    <div className="imgContainer">
+                                        <img src={book.image} alt={`Book cover of ${book.title}`} />
+                                    </div>
+                                    <p>{book.title}</p>
+                                    <p>{book.authors.map((author, index) => {
+                                        return(
+                                            index === 0
+                                            ? author
+                                            : `, ${author}`
+                                        )
+                                    })}</p>
+                                </li>
+                            )
+                        })
+                        : null
+                    }
+                </ul>
+
             </div>
         </div>
     )
